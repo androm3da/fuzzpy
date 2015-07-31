596ba6757925bf8fa6ed4a7ba5142430fb815cff
ebadf <brian.cain@gmail.com>
2015-07-31 00:01:05 -0500

    Add default value for iters

--- run.sh
+++ run.sh
@@ -24,4 +24,4 @@ finish_up()
 
 trap "finish_up $LINENO" INT TERM ERR
 
-${testbin} -runs=${ITERS} -jobs=${JOBS} -use_traces=1 -timeout=10 -max_len=$((16*1024)) -workers=${WORKERS} ${TEST_INPUTS}
+${testbin} -runs=${ITERS-1} -jobs=${JOBS} -use_traces=1 -timeout=10 -max_len=$((16*1024)) -workers=${WORKERS} ${TEST_INPUTS}
