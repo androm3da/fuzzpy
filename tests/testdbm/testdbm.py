ef8c298e6144b22a3fc4ac7b41e78945c39b7887
ebadf <brian.cain@gmail.com>
2015-07-30 19:27:48 -0500

    Flesh out test for gdbm

--- tests/testdbm/testdbm.py
+++ tests/testdbm/testdbm.py
@@ -4,10 +4,24 @@ from dbm import gnu as dbm
 import tempfile
 import os
 
-with tempfile.NamedTemporaryFile(suffix='.db', delete=False) as f:
+FIELDS = (
+    b'NOT',
+    b'Python:',
+    b'',
+    b'REALLY',
+    b'intended',
+    b'THERE',
+)
+with tempfile.NamedTemporaryFile(suffix='.db', delete=False, dir='/dev/shm') as f:
     f.write(b'@')
     f.close()
     try:
         db = dbm.open(f.name, 'r')
+        for f in FIELDS: 
+            try:
+                x = db[f]
+            except:
+                pass
+
     finally:
         os.unlink(f.name)
